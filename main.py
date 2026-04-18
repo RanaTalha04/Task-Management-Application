9db70c2618670174b74af2b98d7258569b1140f3
Muhammad Talha <muhammadtalhashahid2005@gmail.com>
2026-04-18 22:16:16 +0500

    starting backend development

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from src.utils.db import Base, engine
+from src.tasks.models import TaskModel
+
+
+Base.metadata.create_all(engine)
+
+app = FastAPI(title= "This is Task Management Application")
