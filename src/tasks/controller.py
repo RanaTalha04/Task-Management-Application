637638dc08a7ecc9901431a3a6b608279d788967
Muhammad Talha <muhammadtalhashahid2005@gmail.com>
2026-04-20 11:34:08 +0500

    created 'update' and 'delete' api's and tested in 'Postman'

--- src/tasks/controller.py
+++ src/tasks/controller.py
@@ -2,36 +2,77 @@ from src.tasks.dtos import TaskSchema
 from sqlalchemy.orm import session
 from src.tasks.models import TaskModel
 from fastapi import HTTPException
+from typing import List
 
-def create_task(body: TaskSchema, db: session):
 
-    data = body.model_dump()
-    new_task = TaskModel(title = data["title"], description = data["description"], is_completed = data["is_completed"])
+def create_task(body: List[TaskSchema], db: session):
 
-    db.add(new_task)
+    new_tasks = []
+
+    for item in body:
+        data = item.model_dump()
+        task = TaskModel(
+            title=data["title"],
+            description=data["description"],
+            is_completed=data.get("is_completed", False),
+        )
+
+        db.add(task)
+        new_tasks.append(task)
     db.commit()
-    db.refresh(new_task)
 
-    return {"Status": "Tasks created successfully", "data": new_task}
+    for task in new_tasks:
+        db.refresh(task)
 
+    return {"status": "Tasks added successfully", "data": new_tasks}
 
 
 def get_task(db: session):
     tasks = db.query(TaskModel).all()
 
-    return {
-        "status": "Data fetched successfully",
-        "data": tasks
-    } 
+    return {"status": "Data fetched successfully", "data": tasks}
 
 
-def get_one_task(task_id:int, db: session):
-    
+def get_one_task(task_id: int, db: session):
+
     one_task = db.query(TaskModel).get(task_id)
     if not one_task:
-        raise HTTPException(status_code = 404, detail = f"The data with id {task_id} not found")
+        raise HTTPException(
+            status_code=404, detail=f"The data with id {task_id} not found"
+        )
+
+    return {"status": "Task fetched successfully", "data": one_task}
+
+
+def update_task(task_id: int, body: TaskSchema, db: session):
+    task = db.query(TaskModel).get(task_id)
+
+    if not task:
+        raise HTTPException(
+            status_code=404, detail=f"The data with id {task_id} not found"
+        )
+
+    task.title = body.title
+    task.description = body.description
+    task.is_completed = body.is_completed
+
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+
+    return {"status": "Task updated successfully", "data": f"updated task data {task}"}
+
+
+def delete_task(task_id: int, db: session):
+
+    task = db.query(TaskModel).get(task_id)
+
+    if not task:
+        raise HTTPException(
+            status_code=404, detail=f"The data with id {task_id} not found"
+        )
     
-    return {
-        "status": "Task fetched successfully",
-        "data": one_task
-    }
+    db.delete(task)
+    db.commit()
+
+    return {"status": "Task deleted successfully", "data": f"Deleted task {task}"}
--- src/tasks/router.py
+++ src/tasks/router.py
@@ -2,12 +2,14 @@ from fastapi import APIRouter, Depends
 from src.tasks import controller
 from src.tasks.dtos import TaskSchema
 from src.utils.db import get_db
+from typing import List
+
 
 task_routes = APIRouter(prefix="/tasks")
 
 
 @task_routes.post("/create")
-def create_task(body: TaskSchema, db = Depends(get_db)):
+def create_task(body: List[TaskSchema], db = Depends(get_db)):
     
     return controller.create_task(body, db)
 
@@ -22,3 +24,13 @@ def get_task(db = Depends(get_db)):
 def get_one_task(task_id:int, db = Depends(get_db)):
     
     return controller.get_one_task(task_id, db)
+
+@task_routes.put("/update/{task_id}")
+def update_task(task_id: int, body: TaskSchema, db = Depends(get_db)):
+    
+    return controller.update_task(task_id, body, db)
+
+@task_routes.delete("/delete/{task_id}")
+def delete_task(task_id: int, db = Depends(get_db)):
+    
+    return controller.delete_task(task_id, db)
